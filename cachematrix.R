8630b1ded14b6cd032f155c1119e9715f547c055
fatcatflieshigh <emailjingyao@gmail.com>
2017-11-12 22:34:35 -0500

    first

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        inversion <- NULL
+        set <- function(y) {
+                x <<- y
+                inversion <<- NULL
+        }
+        get <- function() x
+        setinversion <- function(inverse) inversion <<- inverse
+        getinversion <- function() inversion
+        list(set=set, get=get, setinversion=setinversion, getinversion=getinversion)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        inversion <- x$getinversion()
+        if(!is.null(inversion)) {
+                message("getting cached data.")
+                return(inversion)
+        }
+        data <- x$get()
+        inversion <- solve(data, ...)
+        x$setinversion(inversion)
+        inversion
+}
